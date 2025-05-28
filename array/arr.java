82760d35a7f815450fc7c7e0be442b4c553b190f
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-05-28 18:41:01 +0530

    Create arr.java

---
+++ array/arr.java
@@ -0,0 +1,11 @@
+public class arr {
+    public static void main(String[] args) {
+        int[]marks=new int[3];
+        marks[0]=87;
+        marks[1]=33;
+        marks[2]=44;
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
