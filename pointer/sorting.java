2fb41a33931d6f410f612a365c71df6beb053e8d
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-05-30 19:32:20 +0530

    Update sorting.java

--- pointer/sorting.java
+++ pointer/sorting.java
@@ -4,5 +4,9 @@ public static void main(String[]args){
 int[] arr={2,4,5,6,7,3,2};
 Arrays.sort(arr);
 System.out.print(Arrays.toString(arr));
+  List<String> names = new ArrayList<>(List.of("Zara", "Bob", "Alice"));
+names.sort((a, b) -> b.compareTo(a)); // descending order
+System.out.println(names); // [Zara, Bob, Alice]
+
 }
 }
