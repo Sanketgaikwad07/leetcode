dee869451829afd5b05bfbcdeb2408f5a17c9e3e
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-08-01 23:09:13 +0530

    Update arr.java

--- array/arr.java
+++ array/arr.java
@@ -2,7 +2,7 @@ public class arr {
     public static void main(String[] args) {
         int[]marks=new int[3];
         marks[0]=87;
-        marks[1]=33;
+        marks[1]=36;
         
         marks[2]=44;
         for(int i=0;i<3;i++){
