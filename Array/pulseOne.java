be9cb2d80e80fbe49e7b5fcb0f9a62f44bb992c3
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-08-04 18:45:53 +0530

    Create pulseOne.java

---
+++ Array/pulseOne.java
@@ -0,0 +1,19 @@
+class pluse{
+  public static void main(String[]args){
+ int [] digit={1,2,3};
+    System.out.println(Arrays.toString(pulseOne(digit)));
+  }
+  static int []pulseOne(int[] digit){
+    int n=digit.length;
+    for(int i=n-1;i>=0;i--){
+      if(digit[i]<9{//check the array no geater then 9
+        digit[i]++;//so is incrumnet the 9+1=10 but 1,0 
+        return digit;
+      }
+      digit[1]=0;
+    }
+    int NewNumbwer=new int [n+1];
+    NewNumber[0]=1;
+    return NewNumbwer;
+  }
+}
