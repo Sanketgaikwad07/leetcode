07a5ba08d67338a8396170d5719698d2ddeed8d8
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-08-05 21:40:36 +0530

    Update pulseOne.java

--- Array/pulseOne.java
+++ Array/pulseOne.java
@@ -3,6 +3,7 @@ class pluse{
  int [] digit={1,2,3};
     System.out.println(Arrays.toString(pulseOne(digit)));
   }
+  
   static int []pulseOne(int[] digit){
     int n=digit.length;
     for(int i=n-1;i>=0;i--){
