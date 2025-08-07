a873aef023f05e744224d0ca7783493e8310ba54
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-08-07 23:19:43 +0530

    Create ReverseInteger.java

---
+++ String/ReverseInteger.java
@@ -0,0 +1,21 @@
+class solution{
+  public static void main(String[]args){
+    rever(213);
+    System.out.println(revse(123));
+  }
+  static int revse(int x){
+    int i=0;
+    while(x!=0){
+      
+      int digit=x%10;
+      
+if(i>Integer.Max_value/10|| i<Integer.Min_Value/10){
+  return  0;
+}
+      
+    i=(i*10)+digit;
+      x=x/10;
+    }
+    retrun i;
+  }
+}
