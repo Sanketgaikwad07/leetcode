70843beda4bc32982477e8ba2156b8cac3a746e8
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-08-09 12:59:25 +0530

    Create Avg.java

---
+++ String/Avg.java
@@ -0,0 +1,13 @@
+class Avg{
+public static void main(String[]args){
+int [] arr={10,89,30,39);
+int sum=0;
+float avg;
+int i=arr.length;
+for(int j:arr){
+sum+=j;
+}
+avg=sum/i;
+System.out.prinln(avg)
+}
+}
