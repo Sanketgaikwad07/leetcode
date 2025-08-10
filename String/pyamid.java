528e6f40f1de86abb25c304f4c43c9c5c5f87c92
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-08-10 19:21:30 +0530

    Create pyamid.java

---
+++ String/pyamid.java
@@ -0,0 +1,13 @@
+class pyramid{
+  public static void main(String[]args){
+    int n=10;
+    for(int i=0;i<=n;i++{
+      for)int j=i;j<n;j++){
+      System.out.println(" ");
+    }
+    for(int k=1;k<=(2*i-1);k++){
+      System.out.pritn("*");
+    }
+    System.out.print();
+  }
+}
