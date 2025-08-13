23ca85a1a951bd562a22835424413a11ffbddc46
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-08-13 22:13:22 +0530

    Create Palindrome.java

---
+++ String/Palindrome.java
@@ -0,0 +1,27 @@
+package String;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str="A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str));
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+
+        }
+        return true;
+
+
+        }
+    }
+
