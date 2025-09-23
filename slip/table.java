36d902829b0fedf369f2d13121d7b5561edced0b
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-09-23 10:37:55 +0530

    Create table.java

---
+++ slip/table.java
@@ -0,0 +1,13 @@
+public class Table {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            System.out.println("Multiplication Table for "+i);
+
+            for(int j=1;j<=100;j++){
+                System.out.println(i+"*"+j+" =  "+i*j);
+
+
+            }
+        }
+    }
+}
