8533ccddb750c02700a6eeec5a037b8d2ceb8121
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-10-28 16:53:01 +0530

    Update funcy.java

--- Array/funcy.java
+++ Array/funcy.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 import java.util.List;
 public class Funcy {
     
+    
     public static void main(String[] args) {
         List<Integer>integerList= Arrays.asList(1,2,3,4,8,8,8,8);
         integerList.stream().map(i->i*i*i).filter(i->i>50).forEach(System.out::println);
