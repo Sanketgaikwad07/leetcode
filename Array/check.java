333edb1af81d3cee15509828dc11c378392710e0
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-11-28 22:06:03 +0530

    Update check.java

--- Array/check.java
+++ Array/check.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class lagest {
 
     
-    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the number");
