0b08092ad8b205960680b33fb6461b22ed5dd0ce
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2026-07-24 21:51:07 +0530

    Update 02_transfer.java

--- 02_transfer.java
+++ 02_transfer.java
@@ -7,6 +7,7 @@ public class StudentRecordDisplay extends JFrame
 {
     private JTextField txtRNo, txtSName, txtPer;
     private JButton btnDisplay;
+    
 
     public StudentRecordDisplay() {
         
